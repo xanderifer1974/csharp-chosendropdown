8b85d63467e273cda054c9c240420842898e399d
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2022-01-09 17:01:52 -0300

    Criação de controller, view e bd do projeto ChosenDropDown e modelagem dropdown

--- ChosenDropDown/Controllers/HomeController.cs
+++ ChosenDropDown/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ChosenDropDown.Models;
 
 namespace ChosenDropDown.Controllers
 {
@@ -26,5 +27,11 @@ namespace ChosenDropDown.Controllers
 
             return View();
         }
+
+        public ActionResult ChosenDropDown()
+        {
+            return View();
+        }
+
     }
 }
